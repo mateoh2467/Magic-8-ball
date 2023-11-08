849cf68e6885deb27a7841fa99baaaf5569bea3d
mateoh2467 <144353650+mateoh2467@users.noreply.github.com>
2023-11-08 22:42:49 +0000

    the ball is ready

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //making random number variable
-let fortuneNumber = (Math.floor(Math.random()*10));
+let fortuneNumber = (Math.floor(Math.random()*(10)+1));
 console.log(fortuneNumber);
 
 //fortune number statements
@@ -8,17 +8,19 @@ if (fortuneNumber === 1) {
 } else if (fortuneNumber === 2) {
     console.log("No");
 } else if (fortuneNumber === 3) {
-    console.log("");
+    console.log("It is uncertain");
 } else if (fortuneNumber === 4 ) {
-    console.log("");
+    console.log("Absolutely");
 } else if ( fortuneNumber === 5) {
-    console.log("");
+    console.log("Almost Certainly");
 } else if (fortuneNumber === 6) { 
-console.log("");
+console.log("Almost Certainly Not");
 } else if (fortuneNumber === 7){
-    console.log("");
+    console.log("With Certainty ");
 } else if (fortuneNumber === 8) {
-    console.log("");
+    console.log("Unclear, try again");
 } else if (fortuneNumber === 9) {
-    console.log("");
+    console.log("The magic eightball says its foretold");
+} else if (fortuneNumber === 10) {
+    console.log("Its not going to happen");
 }
