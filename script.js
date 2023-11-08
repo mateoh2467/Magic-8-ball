b7ca85463b931b9b2eff05ee1dcf51d233af29c3
mateoh2467 <144353650+mateoh2467@users.noreply.github.com>
2023-11-08 15:15:43 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//making random number variable
+let fortuneNumber = (Math.floor(Math.random()*10));
+console.log(fortuneNumber);
+
+//fortune number statements
+if (fortuneNumber === 1) {
+    console.log("Yes");
+} else if (fortuneNumber === 2) {
+    console.log("No");
+} else if (fortuneNumber === 3) {
+    console.log("");
+} else if (fortuneNumber === 4 ) {
+    console.log("");
+} else if ( fortuneNumber === 5) {
+    console.log("");
+} else if (fortuneNumber === 6) { 
+console.log("");
+} else if (fortuneNumber === 7){
+    console.log("");
+} else if (fortuneNumber === 8) {
+    console.log("");
+} else if (fortuneNumber === 9) {
+    console.log("");
+}
